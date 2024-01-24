83b64b813345b5211028eb3ee02912c8c0201c0c
Marina Torunova <masechkacat@gmail.com>
2024-01-24 15:55:28 +0100

    Path fix

--- src/const.js
+++ src/const.js
@@ -1,32 +1,32 @@
 const weatherIcons = {
-    "0": "../public/img/sunny.png", // Ясно
-    "1": "../public/img/cloudy.png", // Малооблачно
-    "2": "../public/img/cloudy.png", // Облачно
-    "3": "../public/img/overcast.png", // Пасмурно
-    "45": "../public/img/fog.png", // Туман
-    "48": "../public/img/fog.png", // Морось
-    "51": "../public/img/freezy.png", // Дождь
-    "53": "../public/img/freezy.png", // Мокрый снег
-    "55": "../public/img/freezy.png", // Снег
-    "56": "../public/img/freezy_drizzle.png", // Снегопад
-    "57": "../public/img/freezy_drizzle.png", // Снег
-    "61": "../public/img/rain.png", // Гроза
-    "63": "../public/img/rain.png", // Гроза
-    "65": "../public/img/rain.png", // Гроза
-    "66": "../public/img/freezy_drizzle.png", // Гроза
-    "67": "../public/img/freezy_drizzle.png", // Гроза
-    "71": "../public/img/snowfall.png.png", // Дождь
-    "73": "../public/img/snowfall.png.png", // Дождь
-    "75": "../public/img/snowfall.png.png", // Дождь
-    "77": "../public/img/snowfall.png.png", // Дождь
-    "80": "../public/img/rain.png", // Дождь
-    "81": "../public/img/rain.png", // Дождь
-    "82": "../public/img/rain.png", // Дождь
-    "85": "../public/img/snowfall.png.png", // Дождь
-    "86": "../public/img/snowfall.png.png", // Дождь
-    "95": "../public/img/thunder.png", // Дождь
-    "96": "../public/img/thunder.png", // Дождь
-    "99": "../public/img/thunder.png", // Дождь
+    "0": "./public/img/sunny.png", // Ясно
+    "1": "./public/img/cloudy.png", // Малооблачно
+    "2": "./public/img/cloudy.png", // Облачно
+    "3": "./public/img/overcast.png", // Пасмурно
+    "45": "./public/img/fog.png", // Туман
+    "48": "./public/img/fog.png", // Морось
+    "51": "./public/img/freezy.png", // Дождь
+    "53": "./public/img/freezy.png", // Мокрый снег
+    "55": "./public/img/freezy.png", // Снег
+    "56": "./public/img/freezy_drizzle.png", // Снегопад
+    "57": "./public/img/freezy_drizzle.png", // Снег
+    "61": "./public/img/rain.png", // Гроза
+    "63": "./public/img/rain.png", // Гроза
+    "65": "./public/img/rain.png", // Гроза
+    "66": "./public/img/freezy_drizzle.png", // Гроза
+    "67": "./public/img/freezy_drizzle.png", // Гроза
+    "71": "./public/img/snowfall.png.png", // Дождь
+    "73": "./public/img/snowfall.png.png", // Дождь
+    "75": "./public/img/snowfall.png.png", // Дождь
+    "77": "./public/img/snowfall.png.png", // Дождь
+    "80": "./public/img/rain.png", // Дождь
+    "81": "./public/img/rain.png", // Дождь
+    "82": "./public/img/rain.png", // Дождь
+    "85": "./public/img/snowfall.png.png", // Дождь
+    "86": "./public/img/snowfall.png.png", // Дождь
+    "95": "./public/img/thunder.png", // Дождь
+    "96": "./public/img/thunder.png", // Дождь
+    "99": "./public/img/thunder.png", // Дождь
 };
 
 const API_URL = {
