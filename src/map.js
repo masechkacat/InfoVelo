259304e9db770aa4baabf33c50f897f80caf459d
Marina Torunova <masechkacat@gmail.com>
2024-01-24 16:00:40 +0100

    Language fix

--- src/map.js
+++ src/map.js
@@ -2,7 +2,7 @@ function displayVelibMarkers(data, map) {
   data.results.forEach(station => {
 
     const marker = L.marker([station.coordonnees_geo.lat, station.coordonnees_geo.lon]).addTo(map);
-    marker.bindPopup(`<b>${station.name}</b><br>Велосипеды: ${station.numbikesavailable}<br>Свободные доки: ${station.numdocksavailable}`).openPopup();
+    marker.bindPopup(`<b>${station.name}</b><br>Velos dispo: ${station.numbikesavailable}<br>Docks dispo: ${station.numdocksavailable}`).openPopup();
   });
 }
 
